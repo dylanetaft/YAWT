2c265122beb7b2536686632c713933307b77269f
Dylan Taft <dylanetaft@gmail.com>
2026-06-15 16:06:20 -0400

    docs attempt

---
+++ docs/doxygen-public/group__QUIC__FRAME__TYPES.js
@@ -0,0 +1,22 @@
+var group__QUIC__FRAME__TYPES =
+[
+    [ "YAWT_Q_Frame_Max_Data_t", "structYAWT__Q__Frame__Max__Data__t.html", null ],
+    [ "YAWT_Q_Frame_Max_Stream_Data_t", "structYAWT__Q__Frame__Max__Stream__Data__t.html", null ],
+    [ "YAWT_Q_Frame_Max_Streams_t", "structYAWT__Q__Frame__Max__Streams__t.html", null ],
+    [ "YAWT_Q_Frame_Data_Blocked_t", "structYAWT__Q__Frame__Data__Blocked__t.html", null ],
+    [ "YAWT_Q_Frame_Stream_Data_Blocked_t", "structYAWT__Q__Frame__Stream__Data__Blocked__t.html", null ],
+    [ "YAWT_Q_Frame_Streams_Blocked_t", "structYAWT__Q__Frame__Streams__Blocked__t.html", null ],
+    [ "YAWT_Q_Frame_New_Connection_ID_t", "structYAWT__Q__Frame__New__Connection__ID__t.html", null ],
+    [ "YAWT_Q_Frame_Retire_Connection_ID_t", "structYAWT__Q__Frame__Retire__Connection__ID__t.html", null ],
+    [ "YAWT_Q_Frame_Path_Challenge_t", "structYAWT__Q__Frame__Path__Challenge__t.html", null ],
+    [ "YAWT_Q_Frame_Path_Response_t", "structYAWT__Q__Frame__Path__Response__t.html", null ],
+    [ "YAWT_Q_Frame_Connection_Close_t", "structYAWT__Q__Frame__Connection__Close__t.html", [
+      [ "reason_phrase", "structYAWT__Q__Frame__Connection__Close__t.html#ab0bfc04602235ed6824ad9c15a230715", null ]
+    ] ],
+    [ "YAWT_Q_Frame_Connection_Close_App_t", "structYAWT__Q__Frame__Connection__Close__App__t.html", [
+      [ "reason_phrase", "structYAWT__Q__Frame__Connection__Close__App__t.html#ae75b825e915a2ffc342f426b20a1d2e2", null ]
+    ] ],
+    [ "YAWT_Q_Frame_Datagram_t", "structYAWT__Q__Frame__Datagram__t.html", [
+      [ "dataptr", "structYAWT__Q__Frame__Datagram__t.html#aeb27c8dc23f07614c6e3a242fd623d29", null ]
+    ] ]
+];
---
+++ docs/doxygen-public/search/groups_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['types_0',['Frame Types',['../group__QUIC__FRAME__TYPES.html',1,'']]]
+];
